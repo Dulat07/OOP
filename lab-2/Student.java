a04d265fe99a26ab86785a92e64ed6d481887598
Dulat07 <141316050+Dulat07@users.noreply.github.com>
2026-02-06 23:27:10 +0500

    Create Student.java

---
+++ lab-2/Student.java
@@ -0,0 +1,25 @@
+package lab1.java;
+
+public class Student {
+    private String name;
+    private String id;
+    private int yearOfStudy;
+
+    public Student(String name, String id) {
+        this.name = name;
+        this.id = id;
+        this.yearOfStudy = 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void incrementYear() {
+        yearOfStudy = yearOfStudy + 1;
+    }
+}
